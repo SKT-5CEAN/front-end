8edb94c0b2e9219f061a950962fc07954f0f35da
정세희 <zanne1218@naver.com>
2024-10-07 23:51:47 +0900

    DEV-52 feat: page.tsx - Fullpage 스크롤링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
+"use client";
+import Fullpage, { FullPageSections, FullpageSection, FullpageNavigation } from '@ap.cx/react-fullpage';
+
 import Header from "@/components/Header/Header";
+import FeatureSection1 from "@/components/sections/FeatureSection1";
+import IntroSection from "@/components/sections/IntroSection";
+import ReviewSection from "@/components/sections/ReviewSection";
 import Image from "next/image";
+import FeatureSection2 from '@/components/sections/FeatureSection2';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Fullpage>
+        <FullPageSections>
+          <FullpageSection>
+            <IntroSection />
+          </FullpageSection>
+          <FullpageSection>
+            <FeatureSection1 />
+          </FullpageSection>
+          <FullpageSection>
+            <FeatureSection2 />
+          </FullpageSection>
+
+          {/* <FullpageSection>
+            <ReviewSection />
+          </FullpageSection> */}
+        </FullPageSections>
+        <FullpageNavigation />
+      </Fullpage>
     </div>
   );
 }
