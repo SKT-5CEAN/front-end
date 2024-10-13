662846f842d37e70b5c2c017b5d65e0764b7097c
정세희 <zanne1218@naver.com>
2024-10-14 00:00:27 +0900

    DEV-63 feat: page.tsx 누락 코드 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
           return (
             <div id="fullpage-wrapper">
               <div className="section h-screen flex justify-center items-center">
+                <IntroSection />
               </div>
               <div className="section h-screen flex justify-center items-center">
                 <FeatureSection1 />
