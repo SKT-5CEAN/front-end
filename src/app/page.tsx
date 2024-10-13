a55f8edc10357d8c6bf417b8e9003852bd8c629f
정세희 <zanne1218@naver.com>
2024-10-14 00:58:47 +0900

    DEV-10 feat: 로그인 상태관리 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,57 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactFullPage from '@fullpage/react-fullpage';
 import Header from "@/components/Header/Header";
 import FeatureSection1 from "@/components/sections/FeatureSection1";
 import IntroSection from "@/components/sections/IntroSection";
 import FeatureSection2 from '@/components/sections/FeatureSection2';
+import LoginComponent from "@/components/LoginComponent";
 
 export default function Home() {
+  const [isLogin, setIsLogin] = useState(false);
+
   useEffect(() => {
     const creditsElement = document.querySelector('.fp-watermark');
     if (creditsElement) {
       creditsElement.remove();
     }
   }, []);
+  const handleLogoClick = () => {
+    console.log("로고 클릭");
+    setIsLogin(false); 
+  }
+
+  const handleLoginClick = () => {
+    console.log("로그인 버튼 클릭");
+    setIsLogin(true);
+  };
   return (
     <div>
-      <Header />
-      <ReactFullPage
-        scrollingSpeed={1000} 
-        navigation={true} 
-        credits={{enabled: false}}
-        render={() => {
-          return (
-            <div id="fullpage-wrapper">
-              <div className="section h-screen flex justify-center items-center">
-                <IntroSection />
+      <Header onLogoClick={handleLogoClick} onLoginClick={handleLoginClick} /> 
+      {isLogin ? (
+        <LoginComponent />
+      ) : (
+        <ReactFullPage
+          scrollingSpeed={1000} 
+          navigation={true} 
+          credits={{enabled: false}}
+          render={() => {
+            return (
+              <div id="fullpage-wrapper">
+                <div className="section h-screen flex justify-center items-center">
+                  <IntroSection />
+                </div>
+                <div className="section h-screen flex justify-center items-center">
+                  <FeatureSection1 />
+                </div>
+                <div className="section h-screen flex justify-center items-center">
+                  <FeatureSection2 />
+                </div>
               </div>
-              <div className="section h-screen flex justify-center items-center">
-                <FeatureSection1 />
-              </div>
-              <div className="section h-screen flex justify-center items-center">
-                <FeatureSection2 />
-              </div>
-            </div>
-          );
-        }}
-      />
+            );
+          }}
+        />
+      )}
     </div>
   );
 }
