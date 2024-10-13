f3e2656213ca15cc772cf67cb85362a402d3859b
정세희 <zanne1218@naver.com>
2024-10-13 23:54:33 +0900

    DEV-63 style: bg gradient 추가

--- src/components/sections/FeatureSection2.tsx
+++ src/components/sections/FeatureSection2.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 export default function FeatureSection2() {
   return (
-    <section className="pt-36 container mx-auto py-20 flex justify-between items-center">
-      <div>
-        <h2 className="text-5xl font-bold text-gray-800">지나간 과정도 소중하게, <br /> 떠올리고 돌아보기</h2>
-        <p className="text-lg text-gray-600 mt-4">나만의 이야기로 남기는 기록</p>
-      </div>
-      <div>
-        <Image src="/images/feature2.png" alt="feature2 이미지" width={800} height={800} />
+    <section className="h-screen w-screen mx-auto flex justify-end items-center bg-gradient-to-b from-[#FCFCFC] from-24% to-[#F9F3FF]">
+      <div className="flex px-auto mr-10">
+        <div className="mt-[10%] mr-10">
+          <h2 className="text-5xl font-bold text-gray-800">지나간 과정도 소중하게, <br /> 떠올리고 돌아보기</h2>
+          <p className="text-lg text-gray-600 mt-4">나만의 이야기로 남기는 기록</p>
+        </div>
+        <div className="mr-20">
+          <Image src="/images/feature2.png" alt="feature2 이미지" width={1000} height={1000} />
+        </div>
       </div>
     </section>
   );
--- src/components/sections/IntroSection.tsx
+++ src/components/sections/IntroSection.tsx
@@ -1,6 +1,6 @@
 export default function IntroSection() {
   return (
-    <section className="flex flex-col items-center justify-center h-screen">
+    <section className="w-screen flex flex-col items-center justify-center h-screen  bg-gradient-to-b from-[#EDF9E0] from-10% to-[#FAF8CF]">
       <h1 className="text-5xl font-bold text-center">취업 준비로 지친 당신을 위한 <br />
         프로세스 관리 툴</h1>
       {/* <p className="text-2xl">취업을 위한 취업 준비 서비스</p> */}
