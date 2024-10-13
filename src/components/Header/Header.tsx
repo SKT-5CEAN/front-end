41a3ee8b80db2f8126294cdbc15045bc4ab977fe
정세희 <zanne1218@naver.com>
2024-10-14 01:00:03 +0900

    DEV-10 feat: 로고 이미지, 로그인 버튼 클릭 시 상태변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,12 +4,13 @@ import Image from "next/image";
 import logo from "../../../public/logo1.png";
 import my from "../../../public/ic-my.png";
 
-function Header() {
+function Header({ onLogoClick, onLoginClick }: { onLogoClick: () => void, onLoginClick: () => void }) {
   const { selectedMenu, setSelectedMenu } = useHeaderStore();
 
   const handleClick = (e: React.MouseEvent<HTMLLIElement>) => {
     const text = e.currentTarget.innerText as HeaderMenu;
     setSelectedMenu(text);
+    console.log(text);
     // 추후 페이지 link 추가
   };
 
@@ -23,7 +24,12 @@ function Header() {
 
   return (
     <header className="fixed top-0 left-0 w-full h-[60px] flex justify-between items-center px-16 bg-white z-50">
-      <Image src={logo} alt="취얼업 로고" className="w-[83.25px] h-[45px]" />
+      <button
+        className="flex items-center"
+        onClick={onLogoClick}
+      >
+        <Image src={logo} alt="취얼업 로고" className="w-[83.25px] h-[45px]" />
+      </button>
       <ul className="flex space-x-28">
         {menuItems.map((menu) => (
           <li
@@ -38,7 +44,12 @@ function Header() {
         ))}
       </ul>
       <div>
-        <button className="border-[2px] border-[#FFE74C] text-[#F5A524] font-bold py-2 px-4 rounded-[8px]">로그인 / 회원가입</button>
+        <button
+          className="border-[2px] border-[#FFE74C] text-[#F5A524] font-bold py-2 px-4 rounded-[8px]"
+          onClick={onLoginClick}  // 로그인 버튼 클릭 핸들러
+        >
+          로그인 / 회원가입
+        </button>
       </div>
     </header>
   );
