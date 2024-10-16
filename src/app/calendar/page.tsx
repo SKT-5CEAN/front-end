b8eeb203d359078d040066abf51680c3a8aee52e
leehyewon0531 <ifpffk@naver.com>
2024-10-16 17:43:43 +0900

    DEV-65 feat: 기본 Calendar 세팅

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,11 @@
+import MainCalendar from "@/components/Calendar/MainCalendar";
+
+function CalendarPage() {
+  return (
+    <div>
+      <MainCalendar />
+    </div>
+  );
+}
+
+export default CalendarPage;
---
+++ src/components/Calendar/MainCalendar.tsx
@@ -0,0 +1,7 @@
+import Calendar from "react-calendar";
+
+function MainCalendar() {
+  return <Calendar />;
+}
+
+export default MainCalendar;
