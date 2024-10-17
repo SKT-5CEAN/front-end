39c81a7d28aa6c613001271e1e5028d4872f8aa3
leehyewon0531 <ifpffk@naver.com>
2024-10-17 19:19:41 +0900

    DEV-65 style: MainCalendar 스타일 작업

--- src/components/Calendar/MainCalendar.tsx
+++ src/components/Calendar/MainCalendar.tsx
@@ -1,7 +1,24 @@
+"use client";
 import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./calendar.css";
 
 function MainCalendar() {
-  return <Calendar />;
+  const formatMonthYear = (locale: string | undefined, date: Date) => {
+    return date.toLocaleString(locale, { month: "long" });
+  };
+
+  return (
+    <Calendar
+      locale="en-US"
+      prev2Label={false}
+      next2Label={false}
+      formatMonthYear={(locale, date) => formatMonthYear(locale, date)}
+      formatShortWeekday={(locale, date) =>
+        date.toLocaleDateString(locale, { weekday: "long" })
+      }
+    />
+  );
 }
 
 export default MainCalendar;
