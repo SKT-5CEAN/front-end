2b54a9846ce4a44a48f5195591b9ec87e82fcf09
leehyewon0531 <ifpffk@naver.com>
2024-10-21 08:41:49 +0900

    DEV-65 fix: Header layout.tsx로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
   return (
     <QueryClientProvider client={queryClient}>
       <div>
-        <Header />
         {accessTkn ? (
           <LoginComponent />
         ) : (
