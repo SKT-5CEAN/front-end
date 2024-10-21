83be3d25878831164955abc81b805e78474e6ac0
leehyewon0531 <ifpffk@naver.com>
2024-10-21 18:09:32 +0900

    DEV-72 refactor: CompanyList store 상태 기반으로 렌더링 하게 수정

--- src/components/domain/apply/CompanyList.tsx
+++ src/components/domain/apply/CompanyList.tsx
@@ -1,13 +1,36 @@
+"use client";
+
 import TitleList from "@/components/TitleList/TitleList";
+import { useCompanyStore } from "@/store/useCompanyStore";
+import { useEffect } from "react";
 
 function CompanyList() {
+  const { inProgressList, setInProgress, completedList, setCompleted } =
+    useCompanyStore();
+
+  useEffect(() => {
+    setInProgress([
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+    ]);
+    setCompleted(["a", "b", "c"]);
+  }, []);
+
   return (
     <div className="flex flex-col justify-between">
-      <TitleList
-        title="지원 중인 기업"
-        list={["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"]}
-      />
-      <TitleList title="지원 완료 기업" list={["a", "b", "c"]} />
+      <TitleList title="지원 중인 기업" list={inProgressList} />
+      <TitleList title="지원 완료 기업" list={completedList} />
     </div>
   );
 }
