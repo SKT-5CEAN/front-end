bec5aae1d99bcd3cec676b7d76e1f054a8377920
leehyewon0531 <ifpffk@naver.com>
2024-10-21 17:49:44 +0900

    DEV-72 chore: scroll test를 위한 데이터 추가

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -4,7 +4,24 @@ function ApplyPage() {
   return (
     <div className="pt-32 pb-4 px-11 flex justify-between">
       <section className="flex flex-col justify-between">
-        <TitleList title="지원 중인 기업" list={["a", "b", "c"]} />
+        <TitleList
+          title="지원 중인 기업"
+          list={[
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+          ]}
+        />
         <TitleList title="지원 완료 기업" list={["a", "b", "c"]} />
       </section>
       <section>
