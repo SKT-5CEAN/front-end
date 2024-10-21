634e95e77a7eac10e11cb4d8c4cd6fa021ee8647
leehyewon0531 <ifpffk@naver.com>
2024-10-21 17:50:26 +0900

    DEV-72 feat: 리스트에서 특정 요소 클릭 시 볼드 처리 및 경로 이동

--- src/components/TitleList/TitleList.tsx
+++ src/components/TitleList/TitleList.tsx
@@ -1,9 +1,34 @@
+"use client";
+import { useState } from "react";
 import { TitleListProps } from "./titleList.type";
+import { useRouter } from "next/navigation";
 
 function TitleList(props: TitleListProps) {
   const { title, list } = props;
+  const router = useRouter();
+  const [selected, setSelected] = useState("");
 
-  return <div className="w-64 h-96 border-4"></div>;
+  return (
+    <div className="w-64 h-96 border-4 flex flex-col py-6 px-8">
+      <h2 className="text-2xl">{title}</h2>
+      {list.length > 0 && (
+        <ul className="h-64 overflow-y-scroll">
+          {list.map((el, idx) => (
+            <li
+              key={idx}
+              className={`text-xl ${el === selected ? "font-bold" : ""}`}
+              onClick={() => {
+                setSelected(el);
+                router.push(`/apply/${el}`);
+              }}
+            >
+              {el}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default TitleList;
