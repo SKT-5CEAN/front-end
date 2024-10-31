d9ad4af56be33518c00c7ddb7017fc1de65ccc34
leehyewon0531 <ifpffk@naver.com>
2024-10-31 13:58:38 +0900

    DEV-82 style: Accordion 기본 스타일 작업

--- src/components/common/Accordion/Accordion.tsx
+++ src/components/common/Accordion/Accordion.tsx
@@ -5,12 +5,19 @@ function Accordion(props: AccordionProps) {
   const { items, triggerFontSize } = props;
 
   return (
-    <Root type="multiple">
+    <Root className="w-full flex flex-col gap-4 p-4" type="multiple">
       {items.length > 0 &&
         items.map((el, idx) => (
-          <Item value={`item-${idx}`} disabled={el.disabled}>
-            <Trigger className={`text-${triggerFontSize}`}>{el.title}</Trigger>
-            <Content>{el.content}</Content>
+          <Item
+            className={`w-full border ${el.disabled ? "bg-gray-300" : "bg-lime-300"} rounded-2xl`}
+            key={idx}
+            value={`item-${idx}`}
+            disabled={el.disabled}
+          >
+            <Trigger className={`text-${triggerFontSize} w-full p-4 text-left	`}>
+              {el.title}
+            </Trigger>
+            <Content className="p-4 bg-lime-500">{el.content}</Content>
           </Item>
         ))}
     </Root>
