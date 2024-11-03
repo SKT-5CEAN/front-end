64c287d4c951a19ff38611693fc7dcd06535457a
leehyewon0531 <ifpffk@naver.com>
2024-11-03 17:07:09 +0900

    DEV-85 feat: 사용자 입력 폼으로 resumeData 업데이트

--- src/components/domain/apply/ResumeItem/ResumeItem.tsx
+++ src/components/domain/apply/ResumeItem/ResumeItem.tsx
@@ -1,9 +1,31 @@
+import Image from "next/image";
 import { ResumeItemProps } from "./resumeItem.type";
 
-function ResumeItem(props: ResumeItemProps) {
-  const { title = "", content = "" } = props;
-
-  return <div>Hi</div>;
+function ResumeItem({ title, content, onChange }: ResumeItemProps) {
+  return (
+    <div className="w-full flex flex-col gap-4 p-6">
+      <div className="w-full flex justify-between">
+        <input
+          className="w-11/12 h-16 text-3xl border border-slate-400 rounded-lg px-3 outline-none"
+          type="text"
+          placeholder="자소서 문항 제목을 입력해 주세요"
+          value={title}
+          onChange={(e) => onChange("question", e.target.value)}
+        />
+        <button className="cursor-pointer">
+          <Image src="/trash-can.png" alt="삭제 버튼" width={40} height={40} />
+        </button>
+      </div>
+      <div className="w-full">
+        <textarea
+          className="w-full h-56 text-2xl border border-slate-400 rounded-lg p-4 outline-none"
+          placeholder="문항에 대한 답변을 작성해 주세요. (문항 제목만 추가하고 싶다면 비워둬도 좋아요)"
+          value={content}
+          onChange={(e) => onChange("content", e.target.value)}
+        ></textarea>
+      </div>
+    </div>
+  );
 }
 
 export default ResumeItem;
--- src/components/domain/apply/ResumeItemList/ResumeItemList.tsx
+++ src/components/domain/apply/ResumeItemList/ResumeItemList.tsx
@@ -1,19 +1,72 @@
-import { useResumeStore } from "@/store/useResumeStore";
-import { useState } from "react";
+import { ResumeDataType, useResumeStore } from "@/store/useResumeStore";
+import { useEffect, useState } from "react";
 import ResumeItem from "../ResumeItem/ResumeItem";
+import PlainButton from "@/components/common/Button/PlainButton/PlainButton";
 
 function ResumeItemList() {
-  const { resumeData } = useResumeStore();
-  const [inputResume, setInputResume] = useState(resumeData);
+  const { resumeData, setResumeData } = useResumeStore();
+  const [inputResume, setInputResume] = useState([
+    ...resumeData,
+    { question: "", content: "" },
+  ]);
+
+  useEffect(
+    function addEmptyForm() {
+      setInputResume([...resumeData, { question: "", content: "" }]);
+    },
+    [resumeData]
+  );
+
+  const handleChange = (
+    idx: number,
+    field: keyof ResumeDataType,
+    value: string
+  ) => {
+    setInputResume((prev) =>
+      prev.map((item, i) => (i === idx ? { ...item, [field]: value } : item))
+    );
+  };
+
+  const saveResumeData = () => {
+    const filteredData = inputResume.filter(
+      (item) => item.question || item.content
+    );
+    setResumeData(filteredData);
+  };
+
+  const cancelEditing = () => {
+    setInputResume([...resumeData, { question: "", content: "" }]); // 원래 데이터로 초기화
+  };
+
+  const addNewResumeItem = () => {
+    setInputResume((prev) => [...prev, { question: "", content: "" }]);
+  };
 
   return (
-    <div>
-      {inputResume &&
-        inputResume.length > 0 &&
-        inputResume.map((el, idx) => (
-          <ResumeItem key={idx} title={el.question} content={el.content} />
+    <div className="w-full h-full">
+      <div>
+        <PlainButton
+          text="작성 취소"
+          textColor="text-red-200"
+          handleClick={cancelEditing}
+        />
+        <PlainButton
+          text="작성 완료"
+          textColor="text-black-200"
+          handleClick={saveResumeData}
+        />
+      </div>
+      <div className="w-full h-5/6 overflow-y-scroll">
+        {inputResume.map((el, idx) => (
+          <ResumeItem
+            key={idx}
+            title={el.question}
+            content={el.content}
+            onChange={(field, value) => handleChange(idx, field, value)}
+          />
         ))}
-      <ResumeItem />
+      </div>
+      <button onClick={addNewResumeItem}>+</button>
     </div>
   );
 }
