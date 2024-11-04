1620a74831319121c16f0e4b16386f2704c773f1
leehyewon0531 <ifpffk@naver.com>
2024-11-04 18:27:46 +0900

    DEV-88 style: 정적 스타일 적용

--- src/components/common/Bar/ProgressBar/ProgressBar.tsx
+++ src/components/common/Bar/ProgressBar/ProgressBar.tsx
@@ -4,10 +4,21 @@ function ProgressBar(props: ProgressBarProps) {
   const { processData } = props;
 
   return (
-    <div className="w-full h-[106px] bg-gray-200 rounded-xl flex">
-      <div></div>
-      {processData.length > 0 &&
-        processData.map((el, idx) => <div key={idx}>{el.name}</div>)}
+    <div className="w-full h-[106px] bg-gray-200 rounded-xl flex justify-center items-center relative">
+      <div className="w-[920px] h-[1px] bg-black absolute"></div>
+      <div className="w-[970px] h-20 flex absolute justify-between">
+        {processData.length > 0 &&
+          processData.map((el, idx) => (
+            <div
+              key={idx}
+              className="h-18 flex flex-col justify-end items-center relative"
+            >
+              <div className="w-8 h-8 rounded-full bg-blue-600 bg-opacity-50 absolute inset-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+              <div className="w-4 h-4 rounded-full bg-black absolute inset-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+              <div>{el.name}</div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
