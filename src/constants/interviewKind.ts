aa747bfdee1f5d1135611f934ccc02a00a2b92df
leehyewon0531 <ifpffk@naver.com>
2024-11-07 16:38:15 +0900

    DEV-89 chore: 면접 질문 종류 객체 형태로 수정

--- src/constants/interviewKind.ts
+++ src/constants/interviewKind.ts
@@ -1,8 +1,32 @@
 export const INTERVIEW_KIND = [
-  "자소서 예상 질문",
-  "기업 관련 질문",
-  "인성 질문",
-  "직무 질문",
-  "개인 질문",
-  "기타 질문",
+  {
+    text: "자소서 예상 질문",
+    active: true,
+    activeLink: "/",
+  },
+  {
+    text: "기업 관련 질문",
+    active: true,
+    activeLink: "/",
+  },
+  {
+    text: "인성 질문",
+    active: false,
+    activeLink: "/",
+  },
+  {
+    text: "직무 질문",
+    active: false,
+    activeLink: "/",
+  },
+  {
+    text: "개인 질문",
+    active: false,
+    activeLink: "/",
+  },
+  {
+    text: "기타 질문",
+    active: false,
+    activeLink: "/",
+  },
 ];
