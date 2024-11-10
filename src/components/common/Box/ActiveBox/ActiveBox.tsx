f65ef227cc4b5112eddcf50c970de7a43a90af96
leehyewon0531 <ifpffk@naver.com>
2024-11-10 15:27:53 +0900

    DEV-89 fix: box 항상 active하게 변경

--- src/components/common/Box/ActiveBox/ActiveBox.tsx
+++ src/components/common/Box/ActiveBox/ActiveBox.tsx
@@ -1,36 +1,18 @@
 "use client";
-import { useState } from "react";
 import { ActiveBoxProps } from "./activeBox.type";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 
 function ActiveBox(props: ActiveBoxProps) {
   const router = useRouter();
-  const { boxText, activeLink, active } = props;
-  const [btnActive, setBtnActive] = useState(active);
+  const { boxText, link } = props;
 
   return (
     <div
-      onClick={
-        btnActive
-          ? () => {
-              router.push(activeLink);
-            }
-          : () => {}
-      }
-      className={`group relative w-[455px] h-36 ${btnActive ? "bg-amber-200" : "bg-gray-200"} flex justify-center items-center`}
+      onClick={() => {
+        router.push(link);
+      }}
+      className={`group relative w-[455px] h-36 bg-amber-200 flex justify-center items-center cursor-pointer`}
     >
-      <button
-        className={`absolute top-3 right-3 w-12 h-12 ${btnActive ? "bg-red-400" : "bg-lime-400"} rounded-2xl flex justify-center items-center hidden group-hover:flex`}
-      >
-        <Image
-          className={`${btnActive ? "" : "rotate-45"}`}
-          src={"/ic-plus.png"}
-          alt="버튼 모양"
-          width={16}
-          height={15}
-        />
-      </button>
       <p className="text-3xl text-center">{boxText}</p>
     </div>
   );
--- src/components/common/Box/ActiveBox/activeBox.type.ts
+++ src/components/common/Box/ActiveBox/activeBox.type.ts
@@ -1,5 +1,4 @@
 export interface ActiveBoxProps {
   boxText: string; // box에 보여질 텍스트
-  activeLink: string; // active 상태일 때 이동할 링크
-  active: boolean;
+  link: string; // 이동할 링크
 }
