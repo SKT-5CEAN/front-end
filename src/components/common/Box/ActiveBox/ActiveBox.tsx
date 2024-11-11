a7f91ae2fb3504b1cc5a2cb76003c5108129cc18
leehyewon0531 <ifpffk@naver.com>
2024-11-11 18:42:29 +0900

    DEV-92 style: 면접 준비 ActiveBox 스타일 작업

--- src/components/common/Box/ActiveBox/ActiveBox.tsx
+++ src/components/common/Box/ActiveBox/ActiveBox.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { useEffect } from "react";
+import PlainButton from "../../Button/PlainButton/PlainButton";
 import { ActiveBoxProps } from "./activeBox.type";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import Image from "next/image";
 
 function ActiveBox(props: ActiveBoxProps) {
   const router = useRouter();
-  const { boxText, kind } = props;
+  const { boxText, boxDesc, kind } = props;
   const pathname = usePathname();
   const params = useSearchParams();
 
@@ -18,9 +19,31 @@ function ActiveBox(props: ActiveBoxProps) {
   return (
     <div
       onClick={handleClick}
-      className={`group relative w-[455px] h-36 bg-amber-200 flex justify-center items-center cursor-pointer`}
+      className={`group relative w-[480px] h-[170px] rounded-xl border border-gray-200 border-t-4 border-t-blue-500 flex flex-col justify-between cursor-pointer p-5`}
     >
-      <p className="text-3xl text-center">{boxText}</p>
+      <div className="flex gap-2">
+        <p className="text-lg font-bold">{boxText}</p>
+        {kind === "resume" && (
+          <div
+            id="chip"
+            className="w-[133px] h-7 p-3 font-medium rounded-full flex justify-between items-center bg-teal-100 text-teal-800 text-xs"
+          >
+            <Image
+              src="/x-diamond-fill.png"
+              alt="AI 질문 생성 아이콘"
+              width={12}
+              height={12}
+            />
+            <p>AI로 질문 생성 가능</p>
+          </div>
+        )}
+      </div>
+      <p className="text-gray-500">{boxDesc}</p>
+      <PlainButton
+        text="대비하기&nbsp;&nbsp;&nbsp;>"
+        textColor="text-blue-500"
+        handleClick={handleClick}
+      />
     </div>
   );
 }
--- src/components/domain/apply/CompanyInterview/CompanyInterview.tsx
+++ src/components/domain/apply/CompanyInterview/CompanyInterview.tsx
@@ -3,9 +3,14 @@ import { INTERVIEW_KIND } from "@/constants/interviewKind";
 
 function CompanyInterview() {
   return (
-    <div className="w-full h-full flex flex-wrap gap-x-12 justify-center items-center pb-14">
+    <div className="w-full h-full flex flex-wrap gap-x-12 gap-y-10 justify-center items-center mt-10">
       {INTERVIEW_KIND.map((el, idx) => (
-        <ActiveBox key={idx} boxText={el.text} kind={el.kind} />
+        <ActiveBox
+          key={idx}
+          boxText={el.text}
+          boxDesc={el.desc}
+          kind={el.kind}
+        />
       ))}
     </div>
   );
