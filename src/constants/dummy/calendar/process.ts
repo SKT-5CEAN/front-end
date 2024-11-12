56a0b9ce5fb17d98be6c4c2eb4d57d33709c5a95
lhw <lhw@innogrid.com>
2024-11-12 11:15:15 +0900

    DEV-105 chore: 예정 일정 더미 데이터 상수화

---
+++ src/constants/dummy/calendar/process.ts
@@ -0,0 +1,49 @@
+import { ProcessChipProps } from "@/components/domain/calendar/ProcessChip/processChip.type";
+
+export const COMPANY_AND_PROCESS: Array<ProcessChipProps> = [
+  {
+    company: "SKT",
+    process: "서류",
+    processColor: "lime-100",
+  },
+  {
+    company: "SKT",
+    process: "코딩테스트",
+    processColor: "purple-200",
+  },
+  {
+    company: "신한은행",
+    process: "코딩테스트",
+    processColor: "purple-200",
+  },
+  {
+    company: "신한은행",
+    process: "면접",
+    processColor: "rose-200",
+  },
+  {
+    company: "네이버",
+    process: "서류",
+    processColor: "lime-100",
+  },
+  {
+    company: "LG CNS",
+    process: "면접",
+    processColor: "rose-200",
+  },
+  {
+    company: "AWS",
+    process: "서류",
+    processColor: "lime-100",
+  },
+  {
+    company: "AWS",
+    process: "인적성검사",
+    processColor: "cyan-100",
+  },
+  {
+    company: "AWS",
+    process: "코딩테스트",
+    processColor: "purple-200",
+  },
+];
