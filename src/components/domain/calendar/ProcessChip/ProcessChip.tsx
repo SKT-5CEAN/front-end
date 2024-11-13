e5f2c37fc0dd4a1fc64ad3de78b20d3a7df4158b
leehyewon0531 <ifpffk@naver.com>
2024-11-13 22:26:58 +0900

    DEV-105 style: ProcessChip 스타일 작업

--- src/components/domain/calendar/ProcessChip/ProcessChip.tsx
+++ src/components/domain/calendar/ProcessChip/ProcessChip.tsx
@@ -11,13 +11,17 @@ function ProcessChip(props: ProcessChipProps) {
     인적성검사: "bg-cyan-200",
   };
 
-  // process에 맞는 색상을 가져오기, 기본값은 'bg-gray-200'
+  // 디폴트 값 부여
   const colorClass = colorMap[process] || "bg-gray-200";
 
   return (
-    <div className="w-full h-[60px] px-[16px] py-[13px] flex justify-between border border-gray-200 rounded">
-      <p>{company}</p>
-      <div className={`${colorClass} p-2 rounded`}>{process}</div>
+    <div className="w-full h-[60px] px-[16px] py-[13px] flex justify-between border border-gray-200 rounded text-center items-center">
+      <p className="font-medium text-xl text-zinc-600">{company}</p>
+      <div
+        className={`${colorClass} px-[15px] py-[5px] rounded font-medium text-lg text-gray-400`}
+      >
+        {process}
+      </div>
     </div>
   );
 }
