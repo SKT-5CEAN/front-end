8fe2e4207178165c96ca85609238c2ab1d43831d
leehyewon0531 <ifpffk@naver.com>
2024-11-15 00:48:28 +0900

    DEV-110 feat: InfoBox 기본 틀 및 props 정의

---
+++ src/components/domain/reviewrecap/InfoBox/InfoBox.tsx
@@ -0,0 +1,9 @@
+import { InfoBoxProps } from "./infoBox.type";
+
+function InfoBox(props: InfoBoxProps) {
+  const { kind } = props;
+
+  return <div></div>;
+}
+
+export default InfoBox;
---
+++ src/constants/infoBoxKind.ts
@@ -0,0 +1,28 @@
+import { InfoType } from "@/components/domain/reviewrecap/InfoBox/infoBox.type";
+
+export const REVIEW_INFO_BOX: InfoType = {
+  title: "잊어버리기 쉬운 취준 과정, 1분 회고로 평생 기억하기",
+  desc: "4단계로 빠르고 간편하게 기록해요. 저장된 회고 내역을 확인할 수 있어요.",
+  bgColor: "bg-yellow-200",
+  titleColor: "text-yellow-800",
+  descColor: "text-yellow-700",
+  iconColor: "bg-rounded-yellow",
+};
+
+export const RECAP_INFO_BOX: InfoType = {
+  title: "복기해서 만드는 나만의 기출모음집",
+  desc: "필기시험에 나왔던 문제와 면접에서 받았던 질문을 기록해요",
+  bgColor: "bg-teal-200",
+  titleColor: "text-teal-800",
+  descColor: "text-teal-700",
+  iconColor: "bg-rounded-teal",
+};
+
+export const BOOKMARK_INFO_BOX: InfoType = {
+  title: "다른 사람은 어떤 후기를 남겼을까?",
+  desc: "태그로 분류하고, 북마크해서 후기글을 모아요.",
+  bgColor: "bg-plainPurple",
+  titleColor: "text-semiBoldPurple",
+  descColor: "text-regularPurple",
+  iconColor: "bg-rounded-purple",
+};
