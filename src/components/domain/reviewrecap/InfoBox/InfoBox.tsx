a9564cc4a96a1d9ea5f8084da316186ac56cdde1
leehyewon0531 <ifpffk@naver.com>
2024-11-15 00:55:10 +0900

    DEV-110 feat: Props에 따라 객체에서 값 가져오기

--- src/components/domain/reviewrecap/InfoBox/InfoBox.tsx
+++ src/components/domain/reviewrecap/InfoBox/InfoBox.tsx
@@ -1,9 +1,22 @@
-import { InfoBoxProps } from "./infoBox.type";
+import {
+  BOOKMARK_INFO_BOX,
+  RECAP_INFO_BOX,
+  REVIEW_INFO_BOX,
+} from "@/constants/infoBoxKind";
+import { InfoBoxProps, InfoType } from "./infoBox.type";
 
 function InfoBox(props: InfoBoxProps) {
   const { kind } = props;
 
-  return <div></div>;
+  const infoBoxMap: Record<InfoBoxProps["kind"], InfoType> = {
+    REVIEW: REVIEW_INFO_BOX,
+    RECAP: RECAP_INFO_BOX,
+    BOOKMARK: BOOKMARK_INFO_BOX,
+  };
+
+  const info = infoBoxMap[kind];
+
+  return <div className={`w-[1002px] h-[84px] ${info.bgColor}`}></div>;
 }
 
 export default InfoBox;
